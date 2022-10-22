b2775b380aeb8d61a70a9fd5cc9dbed2f3b9a64b
Sito8943 <sito8943@gmail.com>
2022-10-21 19:59:27 -0700

    doing 404

--- src/views/NotFound/NotFound.jsx
+++ src/views/NotFound/NotFound.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import { Link } from "react-router-dom";
+
+// sito components
+import SitoContainer from "sito-container";
+
+// @mui/material
+import { Typography, Box } from "@mui/material";
+
+// contexts
+import { useLanguage } from "../../context/LanguageProvider";
 
 const NotFound = () => {
-  return <div></div>;
+  const { languageState } = useLanguage();
+
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        minHeight: "100vh",
+        padding: { lg: "0 300px", md: "0 100px", sm: "0 40px", xs: "0 25px" },
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
+      <SitoContainer flexDirection="column">
+        <Typography variant="h3">
+          {languageState.texts.General.Company}
+        </Typography>
+        <SitoContainer sx={{ marginTop: "30px" }}>
+          <Typography sx={{ fontWeight: "bold", marginRight: "5px" }}>
+            {languageState.texts.NotFound.Title.Bold}
+          </Typography>
+          {languageState.texts.NotFound.Title.Regular}
+        </SitoContainer>
+        <Typography sx={{ marginTop: "30px" }}>
+          {languageState.texts.NotFound.Body}{" "}
+          <Link to="/">{languageState.texts.NotFound.Button}</Link>
+        </Typography>
+      </SitoContainer>
+      <Box
+        sx={{
+          border: "2px solid blue",
+          width: "300px",
+          height: " 300px",
+          display: { md: "flex", xs: "none" },
+        }}
+      ></Box>
+    </Box>
+  );
 };
 
 export default NotFound;
